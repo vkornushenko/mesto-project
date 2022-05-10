308e97d9f7c6ab8d2ed33148b26a188ecc00cbcd
Vladislav Kornushenko <vkornushenko@gmail.com>
2022-05-10 20:34:29 +0300

    исправление ошибок по код ревью

--- script/script.js
+++ script/script.js
@@ -4,107 +4,9 @@ const buttonAddPlaceClose = addPlacePopup.querySelector('.edit-profile__close-bt
 const formElementAddPlace = addPlacePopup.querySelector('.edit-profile');
 const editPlaceName = formElementAddPlace.querySelector('.edit-profile__name');
 const editPlacePic = formElementAddPlace.querySelector('.edit-profile__description');
-
-buttonAddPlaceOpen.addEventListener('click', openAddPlacePopUp);
-buttonAddPlaceClose.addEventListener('click', closeAddPlacePopup);
-formElementAddPlace.addEventListener('submit', formSubmitHandlerAddPlace);
-
-function openAddPlacePopUp(){
-  addPlacePopup.classList.add('pop-up_opened');
-}
-
-function closeAddPlacePopup(){
-  addPlacePopup.classList.remove('pop-up_opened');
-  formElementAddPlace.reset();
-}
-
-// создаем функцию добавления карточки места
-function addCard(title, pic, method){
-
-  const places = document.querySelector('.places');
-  const cardTemplate = document.querySelector('#card').content;
-  // клонируем содержимое шаблона карточки
-  const cardElement = cardTemplate.querySelector('.place').cloneNode(true);
-  // элемент попапа с фото
-  const photoPopup = document.querySelector('.js-open-photo-popup');
-  // элемент фотографии карточки места
-  const placePhotoElement = cardElement.querySelector('.place__photo');
-  // элемент заголовка карточки места
-  const placeNameElement = cardElement.querySelector('.place__name');
-
-  // наполняем содержимым
-  placePhotoElement.src = pic;
-  placePhotoElement.alt = title;
-  placeNameElement.textContent = title;
-
-  // полноразмерный просмотр фото
-  // открытие попапа
-  placePhotoElement.addEventListener('click', function(evt){
-
-    const photoLink = evt.target.getAttribute('src');
-    const placeName = evt.target.nextElementSibling.textContent;
-
-    const popupPicElement = photoPopup.querySelector('.photo-pop-up__image');
-    const popupSubtitleElement = photoPopup.querySelector('.photo-pop-up__subtitle');
-
-    popupPicElement.setAttribute('src', photoLink);
-    popupPicElement.setAttribute('alt', placeName);
-    popupSubtitleElement.textContent = placeName;
-
-    photoPopup.classList.add('pop-up_opened');
-  });
-  // закрытие попапа
-  photoPopup.querySelector('.edit-profile__close-btn').addEventListener('click', function(evt){
-    photoPopup.classList.remove('pop-up_opened');
-  });
-
-  // сюда добавим лайканье
-  cardElement.querySelector('.place__like-btn').addEventListener('click', function(evt){
-    evt.target.classList.toggle('place__like-btn_pressed');
-  });
-
-  // удаление места
-  cardElement.querySelector('.place__delete-btn').addEventListener('click', function(evt){
-    evt.target.parentElement.remove();
-  })
-
-  // отображаем на странице (с учетом последовательности)
-  if(method === 'append'){
-    places.append(cardElement);
-  }
-  else{
-    places.prepend(cardElement);
-  }
-}
-
-
-// создаем карточки из массива карточек
-initialCards.forEach(function(item){
-  addCard(item.name, item.link, 'append');
-});
-
-
-// функция кнопки отправки формы при добавлении нового места
-function formSubmitHandlerAddPlace(evt) {
-  evt.preventDefault();
-  // получаем значения из инпутов
-  let placeName = editPlaceName.value;
-  let placePic = editPlacePic.value;
-  // записываем две переменных в массив "места"
-  let newPlace = {
-    name: placeName,
-    link: placePic
-    };
-  // добавляем новое "место" в начало массива
-  initialCards.unshift(newPlace);
-  // закрываем окно
-  closeAddPlacePopup();
-  // делаем reset для формы
-  formElementAddPlace.reset();
-  addCard(placeName, placePic, 'prepend');
-}
-
-
+// элемент попапа с фото
+const photoPopup = document.querySelector('.js-open-photo-popup');
+const cardTemplate = document.querySelector('#card').content;
 // Редактирование профиля
 // блоки
 const popUp = document.querySelector('.js-edit-profile-popup');
@@ -114,32 +16,127 @@ const profileSubtitleContainer = document.querySelector('.profile__subtitle');
 // кнопки:
 const editProfileButton = document.querySelector('.profile__edit-button');
 const closeProfileButton = popUp.querySelector('.edit-profile__close-btn');
-
+// элементы с именем и профессией профиля
 const nameInput = document.querySelector('.edit-profile__name');
 const jobInput = document.querySelector('.edit-profile__description');
-
+// переменные с именем/профессией профиля
 let profileTitle = profileTitleContainer.textContent;
 let profileSubtitle = profileSubtitleContainer.textContent;
 
+
+buttonAddPlaceOpen.addEventListener('click', openAddPlacePopup);
+buttonAddPlaceClose.addEventListener('click', closeAddPlacePopup);
+formElementAddPlace.addEventListener('submit', formSubmitHandlerAddPlace);
+// слушатель закрытия попапа просмотра фотографии
+photoPopup.querySelector('.edit-profile__close-btn').addEventListener('click', function(){
+  // закрываем попап с фотографией
+  closePopup(photoPopup);
+});
+// слушатели событий для редактирования профиля
+editProfileButton.addEventListener('click', openPopUp);
+closeProfileButton.addEventListener('click', closePopUp);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+function openAddPlacePopup(){
+  openPopup(addPlacePopup);
+}
+function closeAddPlacePopup(){
+  closePopup(addPlacePopup);
+  formElementAddPlace.reset();
+}
+
+// создаем функцию добавления карточки места
+function addCard(title, pic){
+  // клонируем содержимое шаблона карточки
+  const cardElement = cardTemplate.querySelector('.place').cloneNode(true);
+  // элемент фотографии карточки места
+  const placePhotoElement = cardElement.querySelector('.place__photo');
+  // элемент заголовка карточки места
+  const placeNameElement = cardElement.querySelector('.place__name');
+  // наполняем содержимым
+  placePhotoElement.src = pic;
+  placePhotoElement.alt = title;
+  placeNameElement.textContent = title;
+  // полноразмерный просмотр фото
+  // открытие попапа
+  placePhotoElement.addEventListener('click', function(evt){
+    const photoLink = evt.target.getAttribute('src');
+    const placeName = evt.target.nextElementSibling.textContent;
+    const popupPicElement = photoPopup.querySelector('.photo-pop-up__image');
+    const popupSubtitleElement = photoPopup.querySelector('.photo-pop-up__subtitle');
+    popupPicElement.setAttribute('src', photoLink);
+    popupPicElement.setAttribute('alt', placeName);
+    popupSubtitleElement.textContent = placeName;
+    // открываем попап с фотографией
+    openPopup(photoPopup);
+  });
+  // слушатель лайков
+  cardElement.querySelector('.place__like-btn').addEventListener('click', function(evt){
+    evt.target.classList.toggle('place__like-btn_pressed');
+  });
+  // слушатель удаления места
+  cardElement.querySelector('.place__delete-btn').addEventListener('click', function(evt){
+    evt.target.closest('.place').remove();
+  });
+  // возвращаем готовый элемент с карточкой места
+  return cardElement;
+}
+// ф-я публикации карточки
+function renderCard(cardElement, method){
+  const places = document.querySelector('.places');
+  // отображаем на странице (с учетом последовательности)
+  if(method === 'append'){
+    places.append(cardElement);
+  }
+  else{
+    places.prepend(cardElement);
+  }
+}
+
+// функция кнопки отправки формы при добавлении нового места
+function formSubmitHandlerAddPlace(evt) {
+  evt.preventDefault();
+  // получаем значения из инпутов
+  const placeName = editPlaceName.value;
+  const placePic = editPlacePic.value;
+  // закрываем окно
+  closePopup(addPlacePopup);
+  // делаем reset для формы
+  formElementAddPlace.reset();
+  // создаем карточку и отображаем ее
+  cardElement = addCard(placeName, placePic);
+  renderCard(cardElement, 'prepend');
+}
+
+// ф-я закрытия попапа
+function closePopup(popupElement){
+  popupElement.classList.remove('pop-up_opened');
+}
+// ф-я открытия попапа
+function openPopup(popupElement){
+  popupElement.classList.add('pop-up_opened');
+}
+
 // ф-я открытия модального окна редактирования профиля
 function openPopUp(){
-
-  popUp.classList.add('pop-up_opened');
-
+  // открываем попап редактирования профиля
+  openPopup(popUp);
   // добавляем текущие атрибуты полям редактирования профиля
   nameInput.setAttribute('value', profileTitle);
   jobInput.setAttribute('value', profileSubtitle);
-
 }
 
-// ф-я закрытия профиля
+// ф-я закрытия модального окна редактирования профиля
 function closePopUp(){
-  popUp.classList.remove('pop-up_opened');
+  // закрываем попап редактирования профиля
+  closePopup(popUp);
   // удаляем текущие атрибуты полям редактирования профиля
   formElement.reset();
 }
 
-// ф-я отправки формы
+// ф-я отправки формы редактирования профиля
 function formSubmitHandler(evt) {
   evt.preventDefault();
   // сохраняем значения имени и профиля при отправке формы
@@ -152,7 +149,8 @@ function formSubmitHandler(evt) {
   closePopUp();
 }
 
-// обработчики событий для редактирования профиля
-editProfileButton.addEventListener('click', openPopUp);
-closeProfileButton.addEventListener('click', closePopUp);
-formElement.addEventListener('submit', formSubmitHandler);
+// создаем карточки из массива карточек
+initialCards.forEach(function(item){
+  cardElement = addCard(item.name, item.link);
+  renderCard(cardElement, 'append');
+});
